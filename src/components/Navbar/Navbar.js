cce9383b8b2a8f4bec4c5514391c4d0092e8b98b
Juan Fernando Santos <jfernando.santosm@gmail.com>
2022-11-16 16:39:03 -0600

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Projects</a></li>
+                <li><a>Skills</a></li>
+                <li><a>Contact</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/portfolio.js
@@ -0,0 +1,18 @@
+const header = {
+    homepage: '#',
+    title: '<JFSM />'
+}
+
+const about = {
+    name: '',
+    role: '',
+    description: '',
+    resume: '',
+    social: {
+        linkedin: '',
+        github: ''
+    }
+}
+
+export {header, about}
+
